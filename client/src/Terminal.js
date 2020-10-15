bd24f4883f6a9373e004bbf8438d6a5a2b12a29e
alek <alek.br@gmail.com>
2020-10-14 22:27:22 -0700

    basic terminal command handling

--- client/src/Terminal.js
+++ client/src/Terminal.js
@@ -2,13 +2,32 @@ import React from 'react';
 import './Terminal.css';
 import TickerBar from './components/TickerBar'
 
-
 class CommandLine extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+    this.state = {
+      "command": ""
+    }
+  }
+
+  handleKeyDown(e) {
+    if (e.keyCode === 13) {
+      this.setState({"command": ""})
+    } else if ((e.keyCode >= 48 && e.keyCode <= 57) || (e.keyCode >= 65 && e.keyCode <= 90) || (e.keyCode === 32)) {
+      this.setState({"command": this.state.command + e.key})
+    } else if (e.keyCode === 8) {
+      this.setState({"command": this.state.command.slice(0, -1)})
+    }
+  }
+
+  onChange(e) {}
 
   render() {
     return (
       <div className="CommandLine">
-      <input type="text" placeholder="Enter Something"/>
+      <input type="text" value={this.state.command} placeholder="Have a command for me?" onKeyDown={this.handleKeyDown} onChange={this.onChange} />
       </div>
       )
   }
