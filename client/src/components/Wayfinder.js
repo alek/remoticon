450cf23c311af2ab237940f477abe305b5b424aa
alek <alek.br@gmail.com>
2020-11-02 20:12:35 -0800

    switch to actual participant counts

--- client/src/components/Wayfinder.js
+++ client/src/components/Wayfinder.js
@@ -131,7 +131,7 @@ class RoomEntry extends React.Component {
       <div key={"cell-" + this.idx} className="RoomName" onMouseEnter={this.onMouseEnter} onMouseLeave={this.onMouseLeave}>
                           <h2 style={{ color: this.fill }}>{this.title}</h2>
                           <h4>{this.participants} participants</h4>
-                          <RoomOccupancy total={30} active={this.participants} id={this.idx} color={this.fill}/>
+                          <RoomOccupancy total={30} active={this.participants*0.5} id={this.idx} color={this.fill}/>
                           </div>
                           </a>
     )
@@ -152,28 +152,28 @@ class WayfinderGrid extends React.Component {
 
   getRooms(type) {
     if (type === "Workshops") {
-        return [["Live Breaking into Encrypted 3D Printer Firmware", "https://hackaday.io/project/175071-remoticon-breaking-into-3d-printer-firmware"], 
-                  ["Soldering, Nothing To Be Afraid Of!", "https://hackaday.io/project/175072-remoticon-soldering-nothing-to-be-afraid-of"],
-                  ["Introduction to Modular Synthesis using VCV Rack", "https://hackaday.io/project/175074-introduction-to-modular-synthesis-using-vcv-rack"],
-                  ["Prototyping to the Max", "https://hackaday.io/project/175075-remoticon-prototyping-to-the-max"],
-                  ["PCB Reverse Engineering", "https://hackaday.io/project/175076-remoticon-pcb-reverse-engineering"],
-                  ["KiCad to Blender > Photorealistic PCB renders", "https://hackaday.io/project/175076-remoticon-pcb-reverse-engineering"],
-                  ["Tiny ML", "https://hackaday.io/project/175078-remoticon-tiny-ml"],
-                  ["Crowd-Controlled Robots", "https://hackaday.io/project/175079-remoticon-crowd-controlled-robots"],
-                  ["Basics of RF Emissions Debugging", "https://hackaday.io/project/175080-remoticon-basics-of-rf-emissions-debugging"],
-                  ["Introduction to Firmware Reverse Engineering", "https://hackaday.io/project/175081-remoticon-intro-to-firmware-reverse-engineering"],
-                  ["The Hackers Guide to Hardware Debugging", "https://hackaday.io/project/175082-remoticon-the-hackers-guide-to-hardware-debugging"],
-                  ["Remoticon: Learn How to Hack a Car", "https://hackaday.io/project/175126-learn-how-to-hack-a-car-remoticon-workshop"],
-                  ["MachineChat - JEDI One - A Universal Sensor Hub", "https://hackaday.io/project/175085-remoticon-machinechat-a-universal-sensor-hub"],
-                  ["How to 3D Print onto Fabric", "https://hackaday.io/project/175086-remoticon-how-to-3d-print-onto-fabric"],
-                  ["Give Pigweed a Whirl", "https://hackaday.io/project/175167-remoticon-give-pigweed-a-whirl"],
-                  ["Radi-uhoh: What Is This SDR Thing and How Do I Use", "https://hackaday.io/project/175168-radi-uhoh-what-is-this-sdr-thing-and-how-do-i-use"],
-                  ["Finding Sound and Making Microphones", "https://hackaday.io/project/175169-remoticon-finding-sound-and-making-microphones"],
-                  ["Creative Code Experiments", "https://hackaday.io/project/175170-remoticon-creative-code-experiments"],
-                  ["SMD Challenge", "https://hackaday.io/project/175171-remoticon-smd-challenge"],
-                  ["Making Glowy Origami", "https://hackaday.io/project/175172-remoticon-making-glowy-origami"],
-                  ["Circuit Sculpture Workshop", "https://hackaday.io/project/175173-remoticon-circuit-sculpture-workshop"],
-                  ["The Mechanics of FEA", "https://hackaday.io/project/175174-remoticon-the-mechanics-of-fea"]]
+        return [["Live Breaking into Encrypted 3D Printer Firmware", "https://hackaday.io/project/175071-remoticon-breaking-into-3d-printer-firmware", 114], 
+                  ["Soldering, Nothing To Be Afraid Of!", "https://hackaday.io/project/175072-remoticon-soldering-nothing-to-be-afraid-of", 98],
+                  ["Introduction to Modular Synthesis using VCV Rack", "https://hackaday.io/project/175074-introduction-to-modular-synthesis-using-vcv-rack", 65],
+                  ["Prototyping to the Max", "https://hackaday.io/project/175075-remoticon-prototyping-to-the-max", 89],
+                  ["PCB Reverse Engineering", "https://hackaday.io/project/175076-remoticon-pcb-reverse-engineering", 174],
+                  ["KiCad to Blender > Photorealistic PCB renders", "https://hackaday.io/project/175076-remoticon-pcb-reverse-engineering", 99],
+                  ["Tiny ML", "https://hackaday.io/project/175078-remoticon-tiny-ml", 195],
+                  ["Crowd-Controlled Robots", "https://hackaday.io/project/175079-remoticon-crowd-controlled-robots", 166],
+                  ["Basics of RF Emissions Debugging", "https://hackaday.io/project/175080-remoticon-basics-of-rf-emissions-debugging", 131],
+                  ["Introduction to Firmware Reverse Engineering", "https://hackaday.io/project/175081-remoticon-intro-to-firmware-reverse-engineering", 220],
+                  ["The Hackers Guide to Hardware Debugging", "https://hackaday.io/project/175082-remoticon-the-hackers-guide-to-hardware-debugging", 212],
+                  ["Remoticon: Learn How to Hack a Car", "https://hackaday.io/project/175126-learn-how-to-hack-a-car-remoticon-workshop", 159],
+                  ["MachineChat - JEDI One - A Universal Sensor Hub", "https://hackaday.io/project/175085-remoticon-machinechat-a-universal-sensor-hub", 156],
+                  ["How to 3D Print onto Fabric", "https://hackaday.io/project/175086-remoticon-how-to-3d-print-onto-fabric", 75],
+                  ["Give Pigweed a Whirl", "https://hackaday.io/project/175167-remoticon-give-pigweed-a-whirl", 42],
+                  ["Radi-uhoh: What Is This SDR Thing and How Do I Use", "https://hackaday.io/project/175168-radi-uhoh-what-is-this-sdr-thing-and-how-do-i-use", 117],
+                  ["Finding Sound and Making Microphones", "https://hackaday.io/project/175169-remoticon-finding-sound-and-making-microphones", 65],
+                  ["Creative Code Experiments", "https://hackaday.io/project/175170-remoticon-creative-code-experiments", 42],
+                  ["SMD Challenge", "https://hackaday.io/project/175171-remoticon-smd-challenge", 110],
+                  ["Making Glowy Origami", "https://hackaday.io/project/175172-remoticon-making-glowy-origami", 111],
+                  ["Circuit Sculpture Workshop", "https://hackaday.io/project/175173-remoticon-circuit-sculpture-workshop", 172],
+                  ["The Mechanics of FEA", "https://hackaday.io/project/175174-remoticon-the-mechanics-of-fea", 37]]
       } else if (type === "Keynotes") {
           return [["Opening Ceremonies",""], ["Keynote Talk 1",""], ["Keynote Talk 2",""], ["Closing Ceremonies",""]]
       } else if (type === "Rooms") {
@@ -202,7 +202,7 @@ class WayfinderGrid extends React.Component {
                     fill={frontColor} 
                     title={rooms[Math.floor(i/2)][0]} 
                     url={rooms[Math.floor(i/2)][1]} 
-                    participants={30+Math.floor(10*Math.random())} 
+                    participants={rooms[Math.floor(i/2)][2]} 
                     logo={cells.slice(-1)[0]} 
                     key={Math.random()}
                     onMouseEnter={() => { this.setState({animate: this.state.animate.map((val,idx) => {return (Math.floor(i/2)===idx) ? true : false } )})}} 
